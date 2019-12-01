0e71fe4cb0cc93a1f58f48d54d2cfceee933f9ec
Uice Lu <uicestone@gmail.com>
2019-12-01 21:16:42 +0800

    support any sms

--- aliyun-sdk-wrapper.php
+++ aliyun-sdk-wrapper.php
@@ -25,9 +25,36 @@ function aliyun_send_mobile_code($mobile, $code) {
 			->options([
 				'query' => [
 					'PhoneNumbers' => $mobile,
-					'SignName' => '大鱼测试',
+					'SignName' => ALIYUN_SMS_SIGNATURE,
 					'TemplateCode' => ALIYUN_SMS_CODE_VERIFY_MOBILE,
-					'TemplateParam' => json_encode(array('code' => (string)$code, 'product' => '嘉定区洪德楼'))
+					'TemplateParam' => json_encode(array('code' => (string)$code))
+				],
+			])
+			->request();
+
+		return $result;
+
+	} catch(Exception $exception) {
+		error_log($exception->getMessage());
+	}
+}
+
+function aliyun_send_sms($mobile, $template, $params) {
+	AlibabaCloud::accessKeyClient(ALIYUN_ACCESS_KEY, ALIYUN_ACCESS_SECRET)
+		->regionId('cn-hangzhou')
+		->asGlobalClient();
+	try {
+		$result = AlibabaCloud::rpcRequest()
+			->product('Dysmsapi')
+			->version('2017-05-25')
+			->action('SendSms')
+			->method('POST')
+			->options([
+				'query' => [
+					'PhoneNumbers' => $mobile,
+					'SignName' => ALIYUN_SMS_SIGNATURE,
+					'TemplateCode' => $template,
+					'TemplateParam' => json_encode($params, JSON_UNESCAPED_UNICODE)
 				],
 			])
 			->request();
